ba1513f3dabe441f2aa05aac4f49ee5687b75de5
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2026-07-10 09:45:26 +1000

    Delete docs directory

--- docs/js/script.js
+++
@@ -1,9 +0,0 @@
-const menuButton = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-if (menuButton && navLinks) {
-  menuButton.addEventListener("click", function () {
-    navLinks.classList.toggle("is-open");
-    menuButton.setAttribute("aria-expanded", navLinks.classList.contains("is-open"));
-  });
-}
